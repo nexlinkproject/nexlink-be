6a73fa4f9faa8fb0bda5c4380f68fb55c8cc013b
Naufale <imamnaufalseraph@gmail.com>
2024-06-02 15:10:35 +0700

    add column on models

--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -2,12 +2,15 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../utils/db');
 
 const Project = sequelize.define('Project', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
     name: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
     status: { type: DataTypes.STRING, allowNull: false },
     startDate: { type: DataTypes.DATE, allowNull: false },
     endDate: { type: DataTypes.DATE, allowNull: false },
     deadline: { type: DataTypes.DATE, allowNull: false },
+    createdAt: { type: DataTypes.DATE, allowNull: false },
+    updatedAt: { type: DataTypes.DATE, allowNull: false },
 });
 
 module.exports = Project;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,10 +2,15 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../utils/db');
 
 const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
     username: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, primaryKey: true, allowNull: false, unique: true },
     fullName: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
+    createdAt: { type: DataTypes.DATE, allowNull: false },
+    updatedAt: { type: DataTypes.DATE, allowNull: false },
+    resetPasswordToken: { type: DataTypes.STRING },
+    resetPasswordExpires: { type: DataTypes.DATE },
 });
 
 module.exports = User;
