e3d974818c3350d68523f808f3dc80f8633faa6c
Naufale <imamnaufalseraph@gmail.com>
2024-06-02 10:32:52 +0300

    remove unused user function

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { getUsers, getUserById, updateUser } = require('../controllers/userController');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
 
 module.exports = router;
