85f1a055f963d32192c87cc74098c504ca636263
Naufale <imamnaufalseraph@gmail.com>
2024-06-04 17:46:37 +0300

    fixing response

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { authenticate } = require('../utils/middleware')
 const { login, logout, register, resetPassword } = require('../controllers/authController');
 
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout', authenticate, logout);
 router.post('/register', register);
 router.put('/reset-password', resetPassword);
 
